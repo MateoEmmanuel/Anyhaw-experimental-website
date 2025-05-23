7d1e83179016654db1c3c9dbdeb7b08834f2998b
MateoEmmanuel <mateoemmanuelwalter11@gmail.com>
2025-05-23 10:54:21 +0800

    Delete new_anyhaw directory

--- new_anyhaw/backend/cashier/cashier_system.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template, send_file, redirect, url_for, session, jsonify
-from backend.dbconnection import create_connection
-
-cashier_system_bp = Blueprint('cashier_system_bp', __name__)
-
-@cashier_system_bp.route('/cashier_system_loader')
-def cashier_system_loader():
-    return render_template('cashier_system.html')
--- new_anyhaw/backend/logout.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, redirect, session, url_for
-
-logout_bp = Blueprint('logout_bp', __name__)
-
-
-@logout_bp.route('/logout', methods=['POST'])
-def logout_sys():
-    session.clear()
-    return redirect(url_for('Index_home'))
